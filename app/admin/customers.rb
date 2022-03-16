1d867dc4ef86ace135798754263338d7e4b7458d
Joel <jvicente@aceproject.space>
2022-03-16 13:55:36 -0500

    Added images

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,18 +1,13 @@
 ActiveAdmin.register Customer do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  permit_params :name, :phone, :email, :notes
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :phone, :email, :notes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :name, :phone, :email, :notes, :image
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.actions
+    f.inputs do
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+    end
+  end
 end
