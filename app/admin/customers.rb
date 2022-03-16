0e042c2cb4dcd8068623a104b8af9ead71bfe419
Joel <jvicente@aceproject.space>
2022-03-16 14:36:36 -0500

    Added image to individual customer view

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -10,4 +10,18 @@ ActiveAdmin.register Customer do
       f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
     end
   end
+
+  show do
+    attributes_table do
+      row :name
+      row :phone
+      row :email
+      row :notes
+      row :image do |ad|
+        image_tag ad.image
+      end
+    end
+    active_admin_comments
+  end
+  
 end
